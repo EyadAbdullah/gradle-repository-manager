b6000104c0c9fabf21037079862d01277296b704
Moritz <keocra@gmail.com>
2025-09-03 10:46:05 +0200

    allow requiring authentication for repositories

--- src/test/java/io/github/eyadabdullah/gradlerepositorymanager/RepositoryManagerServiceTest.java
+++ src/test/java/io/github/eyadabdullah/gradlerepositorymanager/RepositoryManagerServiceTest.java
@@ -1,15 +1,29 @@
 package io.github.eyadabdullah.gradlerepositorymanager;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import org.gradle.api.Action;
+import org.gradle.api.artifacts.dsl.RepositoryHandler;
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
 import org.gradle.testkit.runner.GradleRunner;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+
+import io.github.eyadabdullah.gradlerepositorymanager.exceptions.MissingRepositoryCredentials;
+import io.github.eyadabdullah.gradlerepositorymanager.extension.ManageableRepository;
+import io.github.eyadabdullah.gradlerepositorymanager.extension.ManageableRepositoryImpl;
 
 import static java.util.Map.entry;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
 
 @SuppressWarnings("java:S2699")
 class RepositoryManagerServiceTest extends RepositoryManagerBaseTest {
@@ -190,5 +204,132 @@ class RepositoryManagerServiceTest extends RepositoryManagerBaseTest {
         .withPluginClasspath();
     // assert
     assertThrows(Exception.class, result::build);
+  }
+
+  @Test
+  void addRepository_fails_noCredentialsFoundButRepositoryRequiresAuthentication() {
+    // arrange
+    var repositoryManagerService = new RepositoryManagerService();
+
+    var repoHandler = mock(RepositoryHandler.class);
+    mockMavenCall(repoHandler);
+
+    ManageableRepository repository = new ManageableRepositoryImpl("someName");
+    repository.setUrl("https://gitlab.example.com/api/v4/groups/680/-/packages/maven");
+    repository.setRequireAuthentication(true);
+
+    // act
+    var result = assertThatThrownBy(() -> repositoryManagerService.addRepository(repoHandler, repository));
+
+    // assert
+    result.isInstanceOf(MissingRepositoryCredentials.class);
+  }
+
+  @Test
+  void addRepository_fails_credentialsFoundWithoutTokenAndPasswordButRepositoryRequiresAuthentication() {
+    // arrange
+    var repositoryManagerService = new RepositoryManagerService();
+
+    var repoHandler = mock(RepositoryHandler.class);
+    mockMavenCall(repoHandler);
+
+    ManageableRepository repository = new ManageableRepositoryImpl("someName");
+    repository.setUrl("https://gitlab.example.com/api/v4/groups/680/-/packages/maven");
+    repository.setRequireAuthentication(true);
+
+    var credential = new RepositoryCredentials("repository_manager_repo_name_url");
+    credential.setUrl(repository.getUrl());
+
+    repositoryManagerService.setRepositoryCredentials(List.of(credential));
+
+    // act
+    var result = assertThatThrownBy(() -> repositoryManagerService.addRepository(repoHandler, repository));
+
+    // assert
+    result.isInstanceOf(MissingRepositoryCredentials.class);
+  }
+
+  private static List<? super MavenArtifactRepository> mockMavenCall(RepositoryHandler repoHandler) {
+    List<? super MavenArtifactRepository> mavenArtifactRepositories = new ArrayList<>();
+    doAnswer(args -> {
+        Action<? super MavenArtifactRepository> action = args.getArgument(0);
+        var repo = mock(MavenArtifactRepository.class);
+        action.execute(repo);
+        mavenArtifactRepositories.add(repo);
+        return repo;
+      })
+      .when(repoHandler)
+      .maven(ArgumentMatchers.<Action<? super MavenArtifactRepository>>any());
+    return mavenArtifactRepositories;
+  }
+
+  @Test
+  void addRepository_successful_tokenCredentialsFoundAndRepositoryRequiresAuthentication() {
+    // arrange
+    var repoHandler = mock(RepositoryHandler.class);
+    var processedRepositories = mockMavenCall(repoHandler);
+
+    ManageableRepository repository = new ManageableRepositoryImpl("someName");
+    repository.setUrl("https://gitlab.example.com/api/v4/groups/680/-/packages/maven");
+    repository.setRequireAuthentication(true);
+
+    var repositoryManagerService = new RepositoryManagerService();
+
+    var credential = new RepositoryCredentials("repository_manager_repo_name_url");
+    credential.setUrl(repository.getUrl());
+    credential.setTokenName("token_name");
+    credential.setTokenValue("token_value");
+
+    repositoryManagerService.setRepositoryCredentials(List.of(credential));
+
+    // act
+    repositoryManagerService.addRepository(repoHandler, repository);
+
+    // assert
+    assertThat(processedRepositories).hasSize(1);
+  }
+
+  @Test
+  void addRepository_successful_passwordCredentialsFoundAndRepositoryRequiresAuthentication() {
+    // arrange
+    var repoHandler = mock(RepositoryHandler.class);
+    var processedRepositories = mockMavenCall(repoHandler);
+
+    ManageableRepository repository = new ManageableRepositoryImpl("someName");
+    repository.setUrl("https://gitlab.example.com/api/v4/groups/680/-/packages/maven");
+    repository.setRequireAuthentication(true);
+
+    var repositoryManagerService = new RepositoryManagerService();
+
+    var credential = new RepositoryCredentials("repository_manager_repo_name_url");
+    credential.setUrl(repository.getUrl());
+    credential.setUsername("token_name");
+    credential.setPassword("token_value");
+
+    repositoryManagerService.setRepositoryCredentials(List.of(credential));
+
+    // act
+    repositoryManagerService.addRepository(repoHandler, repository);
+
+    // assert
+    assertThat(processedRepositories).hasSize(1);
+  }
+
+  @Test
+  void addRepository_successful_noCredentialsFoundAndRepositoryNotRequiresAuthentication() {
+    // arrange
+    var repoHandler = mock(RepositoryHandler.class);
+    var processedRepositories = mockMavenCall(repoHandler);
+
+    ManageableRepository repository = new ManageableRepositoryImpl("someName");
+    repository.setUrl("https://gitlab.example.com/api/v4/groups/680/-/packages/maven");
+
+    var repositoryManagerService = new RepositoryManagerService();
+
+    // act
+    repositoryManagerService.addRepository(repoHandler, repository);
+
+    // assert
+    assertThat(processedRepositories).hasSize(1);
   }
 }
